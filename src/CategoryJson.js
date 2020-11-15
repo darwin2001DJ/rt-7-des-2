82a78c0c1ac71b548a88962d00007ca56e0fe084
Your Name <darwinmuthu450@gmail.com>
2020-11-15 12:00:19 +0530

    json data added

---
+++ src/CategoryJson.js
@@ -0,0 +1,16 @@
+export let language = [
+  { id: "l1", value: "Tamil" },
+  { id: "l2", value: "English" },
+  { id: "l3", value: "Hindi" },
+];
+export let gener = [
+  { id: "g1", value: "Action" },
+  { id: "g2", value: "Comedy" },
+  { id: "g3", value: "Horror" },
+  { id: "g4", value: "Commercial" },
+  { id: "g5", value: "Disney" },
+];
+export let format = [
+  { id: "f1", value: "2D" },
+  { id: "f2", value: "3D" },
+];
