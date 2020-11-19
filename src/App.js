b3a9ccf2fc488a2e0b9a200b76795ae97407d713
Your Name <darwinmuthu450@gmail.com>
2020-11-19 08:23:08 +0530

    nav-link is working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { language, gener, format } from "./CategoryJson";
+import {
+  nowShowing,
+  comingSoon,
+  exclusive,
+  theaterList,
+} from "./MovieListJSON";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      data: nowShowing,
+    };
+  }
+
+  linkHandler = (opt) => {
+    this.setState({
+      data: opt,
+    });
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <>
+        <ul className="link-container">
+          <li id="nowShowing" onClick={() => this.linkHandler(nowShowing)}>
+            Now Showing
+          </li>
+          <li id="comingSoon" onClick={() => this.linkHandler(comingSoon)}>
+            Coming Soon
+          </li>
+          <li id="exclusive" onClick={() => this.linkHandler(exclusive)}>
+            Exclusive
+          </li>
+        </ul>
+        <div className="container">
+          <div className="category">
+            <span id="language">Select Language</span>
+            <br />
+            {language.map((lang) => {
+              return (
+                <>
+                  <input type="checkbox" id={lang.id} />
+                  <label htmlFor={lang.id}>{lang.value}</label>
+                </>
+              );
+            })}
+
+            <br />
+            <span id="gener">Select Gener</span>
+            <br />
+            {gener.map((gen) => {
+              return (
+                <>
+                  <input type="checkbox" id={gen.id} />
+                  <label htmlFor={gen.id}>{gen.value}</label>
+                </>
+              );
+            })}
+            <br />
+            <span id="format">Select Format</span>
+            <br />
+            {format.map((format) => {
+              return (
+                <>
+                  <input type="checkbox" id={format.id} />
+                  <label htmlFor={format.id}>{format.value}</label>
+                </>
+              );
+            })}
+          </div>
+          <div className="image">
+            {data.map((now, index) => {
+              return (
+                <>
+                  <img src={now.src} alt={index + 1} />
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
